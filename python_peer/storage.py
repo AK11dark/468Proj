f7139b4716089354fc1de7c8c4d6098562d69886
AK11dark <87881098+AK11dark@users.noreply.github.com>
2025-04-07 23:57:19 -0400

    Delete python_peer directory

--- python_peer/storage.py
+++
@@ -1,4 +0,0 @@
-#not implemented yet
-#Saves encrypted files to disk.
-#Loads encrypted files before decryption.
-#Manages database entries for file metadata.
--- python_peer/verification.py
+++
@@ -1,3 +0,0 @@
-#not implemented yet
-#Verifies that files haven’t been tampered with using AES-GCM authentication tags.
-#Rejects corrupted or modified files.
